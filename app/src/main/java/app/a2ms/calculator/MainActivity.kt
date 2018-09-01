e46eed7c4d90e0dda9c43f5b27ebd8c129391c3f
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-01 12:31:53 +0100

    Add screen configuration state save

--- app/src/main/java/app/a2ms/calculator/MainActivity.kt
+++ app/src/main/java/app/a2ms/calculator/MainActivity.kt
@@ -6,16 +6,20 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+//value nd key for screen configuration change
+private const val STATE_PENDING_OPERATION = "PendingOperation"
+private const val STATE_OPERAND1 = "Operand1"
+private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var result: EditText
     private lateinit var newNumber: EditText
     private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
 
     //    Variable to hold the operands and type of calculation
     private var operand1: Double? = null
-    private var operand2: Double = 0.0
     private var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -84,23 +88,43 @@ class MainActivity : AppCompatActivity() {
         if (operand1 == null) {
             operand1 = value
         } else {
-            operand2 = value
             if (pendingOperation == "=") {
                 pendingOperation = operation
             }
             when (pendingOperation) {
-                "=" -> operand1 = operand2
-                "/" -> if (operand2 == 0.0) {
-                    operand1 = Double.NaN //Handle attempt to divide by zero
+                "=" -> operand1 = value
+                "/" -> operand1 = if (value == 0.0) {
+                    Double.NaN //Handle attempt to divide by zero
                 } else {
-                    operand1 = operand1!! / operand2
+                    operand1!! / value
                 }
-                "*" -> operand1 = operand1!! * operand2  //!! bang bang operation ☺
-                "-" -> operand1 = operand1!! - operand2
-                "+" -> operand1 = operand1!! + operand2
+                "*" -> operand1 = operand1!! * value  //!! bang bang operation ☺
+                "-" -> operand1 = operand1!! - value
+                "+" -> operand1 = operand1!! + value
             }
         }
-        result.setText(operand1.toString())
+        result.setText(operand1.toString()) //not a good idea to display number as string for internationalisation
         newNumber.setText("")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        if (operand1 != null) {
+            outState?.putDouble(STATE_OPERAND1, operand1!!)
+            outState?.putBoolean(STATE_OPERAND1_STORED, true)
+        }
+        outState?.putString(STATE_PENDING_OPERATION, pendingOperation)
+    }
+
+    //Parameter change to non nullable
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        operand1 = if (savedInstanceState.getBoolean(STATE_OPERAND1_STORED, false)) {
+            savedInstanceState.getDouble(STATE_OPERAND1)
+        } else {
+            null
+        }
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION)
+        displayOperation.text = pendingOperation
     }
 }
